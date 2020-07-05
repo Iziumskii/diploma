c85db172127160bbcd5bb4ce11ec627c83a04294
Kirill Iizumskii <mr.llirus@gmail.com>
2020-07-05 15:56:48 +0300

    create json

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,10 @@
 from pprint import pprint
 import requests
 import time
-
-#Получить список групп
-#Получить список друзей
-#цикл - Получить спис участ групп - Срав участ гр со списк др - Если нет - сохр. если да - пропуст
-#Вывести группы
+import json
 
 TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-#id = 171691064
-id = 23408383
+id = 171691064
 
 def take_groups(token, usid):
     response = requests.get(
@@ -35,13 +30,13 @@ def take_friends(token, usid):
     print('---')
     return (response.json())
 
-
 def take_groups_members(token, grid):
     response = requests.get(
         'https://api.vk.com/method/groups.getMembers',
         params={
             'access_token': token,
             'group_id': grid,
+            #'filter': 'friends',
             'v': 5.103
         }
     )
@@ -60,45 +55,51 @@ def take_groups_names(token, grid):
     print('---')
     return (response.json())
 
+def sort_groups(groups_list, friends_list):
+    members_list = []
+    final_list = []
+    for group in groups_list:
+        try:
+            members_list.extend(take_groups_members(TOKEN, group)["response"]["items"])
+            set_members_list = set(members_list)
+            if set_members_list.isdisjoint(friends_list) == True:
+                final_list.append(group)
+        except KeyError:
+            print('ошибка')
+        members_list.clear()
+    return final_list
 
-friends_list = []
-groups_list = []
-final_list = []
-l = []
+def create_final_list(final_list):
+    time.sleep(3)
+    names_list = []
+    last_list = []
+    for group in final_list:
+        try:
+            time.sleep(3)
+            names_list.append(take_groups_names(TOKEN, group)["response"][0])
+            last_list.append({"name": names_list[0]["name"], "gid": names_list[0]["id"],
+                              "members_count": take_groups_members(TOKEN, group)["response"]["count"]})
+            names_list.clear()
+        except KeyError:
+            print('ошибка')
+    return last_list
 
-friends_list.extend(take_friends(TOKEN, id)["response"]["items"])
-set_list_friends = set(friends_list)
-pprint(set_list_friends)
+def create_frends_list():
+    friends_list = []
+    friends_list.extend(take_friends(TOKEN, id)["response"]["items"])
+    set_list_friends = set(friends_list)
+    return set_list_friends
 
-groups_list.extend(take_groups(TOKEN, id)["response"]["items"])
-pprint(groups_list)
+def create_groups_list():
+    groups_list = []
+    groups_list.extend(take_groups(TOKEN, id)["response"]["items"])
+    return groups_list
 
+def create_group_data():
+    return create_final_list(sort_groups(create_groups_list(), create_frends_list()))
 
-for i in groups_list:
-    try:
-        l.extend(take_groups_members(TOKEN, i)["response"]["items"])
-        set_l = set(l)
-        check_friends = set_l.isdisjoint(set_list_friends)
-        if check_friends == False:
-            final_list.append(i)
-        l.clear()
-    except KeyError:
-        print('ошибка')
+def create_groups_json_file():
+    with open("groups.json", "w") as file:
+        json.dump((create_group_data()), file)
 
-print(final_list)
-
-time.sleep(3)
-
-d = []
-last_list = []
-
-for i in final_list:
-    try:
-        time.sleep(3)
-        d.append(take_groups_names(TOKEN, i)["response"][0])
-        last_list.append({"name":d[0]["name"], "gid":d[0]["id"],
-                          "members_count":take_groups_members(TOKEN, i)["response"]["count"]})
-        d.clear()
-    except KeyError:
-        print('ошибка')
-pprint(last_list)
+create_groups_json_file()
